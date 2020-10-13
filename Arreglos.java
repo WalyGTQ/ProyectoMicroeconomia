f0d9bd3b2e720d86b913f3e313874d07b7a6ec4a
walyec <52383359+walyec@users.noreply.github.com>
2020-10-12 20:09:37 -0600

    Create Arreglos.java
    Esta clase es nuestra funcion principal, aca es donde tenemos nuestra clase main y en ella se ejecuta y llama a las otras dos clases, Captura y creditos

---
+++ Arreglos.java
@@ -0,0 +1,14 @@
+
+package Arreglos;
+
+
+public class Arreglo {
+    public static void main(String[] args) {
+        
+        Captura V1 = new Captura();
+        V1.setVisible(true);
+        
+        //Modulos V2 = new Modulos();
+        //V2.setVisible(true); 
+    }
+}
